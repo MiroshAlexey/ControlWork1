8ae205f2fac5f95a84e118caae6928043cbbbb4a
MiroshAlexey <166262688+MiroshAlexey@users.noreply.github.com>
2024-05-04 11:56:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите массив строк через пробел:");
+string input = Console.ReadLine();
+string[] array = input.Split(' ');
+string[] newArray = new string[array.Length];
+int newArrayIndex = 0;
+    for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+                {
+                    newArray[newArrayIndex] = array[i];
+                    newArrayIndex++;
+                }
+        }
+Console.WriteLine("Новый массив строк:");
+    for (int i = 0; i < newArray.Length; i++)
+        {
+    Console.Write($"{newArray[i]} ");
+        }
